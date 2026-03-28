131cf4b436cc07e5614bf25e5070eb88ce9285bb
Md Rashadul Islam <rashadul.dev@gmail.com>
2026-03-28 14:01:30 +0600

    fix: get and delete subscriber server action implement

--- src/actions/subscribers/subscribers.ts
+++ src/actions/subscribers/subscribers.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { apiClient } from "@/lib/api";
+import { getAccessToken } from "../auth";
 
 // post Subscribe to newsletter
 export async function subscribeNewsletter({ email }: { email: string }) {
@@ -10,5 +11,33 @@ export async function subscribeNewsletter({ email }: { email: string }) {
     body: { email },
     params: { email },
   });
+  return res;
+}
+
+// get all subscribers - admin
+export async function getAllSubscribers({ isActive }: { isActive?: boolean }) {
+  const token = await getAccessToken();
+
+  const res = await apiClient({
+    endpoint: "/subscribers",
+    method: "GET",
+    params: isActive !== undefined ? { isActive: String(isActive) } : {},
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res;
+}
+
+// delete subscriber - admin
+export async function deleteSubscriber({ id }: { id: string }) {
+  const token = await getAccessToken();
+
+  const res = await apiClient({
+    endpoint: `/subscribers/${id}`,
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
   return res;
 }
